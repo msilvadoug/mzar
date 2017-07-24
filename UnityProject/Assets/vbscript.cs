f21d059f8369819656ae68a92e69a854938c5252
msilvadoug <msilva.doug@gmail.com>
2017-07-23 23:28:31 -0300

    First commit
    Add Unity Project

---
+++ UnityProject/Assets/vbscript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbscript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject vbButtonObject;
+	public GameObject Cube;
+	private GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+
+		vbButtonObject = GameObject.Find ("actionButton");
+		vbButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	
+	}
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+
+
+			
+		Destroy(Player, 5f);
+
+
+		Debug.Log ("Ta funfando");
+	}
+
+	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb) {
+	
+	}
+}
